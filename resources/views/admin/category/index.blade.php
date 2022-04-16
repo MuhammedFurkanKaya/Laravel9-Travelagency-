14d4c6d55ef89eec48596b8ad35b8b8fc48cc48a
MuhammedFurkanKaya <frknkybm@gmail.com>
2022-04-16 04:29:07 +0300

    Category Get and List Data from Database

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -14,12 +14,44 @@
                 </div>
             </div>
             <!-- /. ROW  -->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="alert alert-info">
-                        This is a free responsive admin under cc3.0 license, so you can use it for personal and commercial use.
-                        <br />
-                        Enjoy this admin and for more please keep looking <a href="http://www.binarytheme.com/" target="_blank">BinaryTheme.com</a>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Category List
+                </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Title</th>
+                                <th>Keywords</th>
+                                <th>Description</th>
+                                <th>Image</th>
+                                <th>Status</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                                <th>Show</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach( $data as $rs)
+                            <tr>
+                                <td>{{$rs->id}}</td>
+                                <td>{{$rs->title}}</td>
+                                <td>{{$rs->keywords}}</td>
+                                <td>{{$rs->description}}</td>
+                                <td>{{$rs->image}}</td>
+                                <td>{{$rs->status}}</td>
+                                <td><a href="/admin/category/edit{{$rs->id}}" class="btn btn-sm btn-info">Edit</a> </td>
+                                <td><a href="/admin/category/delete{{$rs->id}}" class="btn btn-sm btn-danger" >Delete</a> </td>
+                                <td><a href="/admin/category/show{{$rs->id}}" class="btn btn-sm btn-success">Show</a> </td>
+
+                            </tr>
+
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
